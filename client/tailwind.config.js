94da08aa5dc26c58e1397776f84f3f37363edd78
Abdelrahman Sameh <abdosamehdev@gmail.com>
2024-09-25 03:14:08 +0300

    first commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,32 @@
+import { flowbite } from "flowbite-react/tailwind";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  theme: {
+    extend: {
+      colors: {
+        // Light theme colors
+        light: {
+          background: "#FFFFFF",
+          secondaryBackground: "#F0F8FF",
+          primaryText: "#004D4D",
+          secondaryText: "#008080",
+          accent: "#7FFFD4",
+        },
+        // Dark theme colors
+        dark: {
+          background: "#003333",
+          secondaryBackground: "#004D4D",
+          primaryText: "#F0F8FF",
+          secondaryText: "#7FFFD4",
+          accent: "#00CED1",
+        },
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [flowbite.plugin()],
+};
