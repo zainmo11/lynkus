4310ab237bd09927817efbc3838567bb2e969eab
Ahmad Fawzy <shadow0181456@gmail.com>
2024-09-29 03:57:13 +0300

    Sidebar finished - Static

--- client/src/components/Head.jsx
+++ client/src/components/Head.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-
 function Head() {
   return (
-    <div className='flex items-center justify-start gap-2'>
-        <img
-            src="logo.png"
-            className="h-14 rounded-full object-cover"
-            alt="Logo"
-        />
-            <div className='flex flex-col'>
-                <p className='font-semibold'>Sameh</p>
-                <p className='font-thin'>@3abkr</p>
-            </div>
+    <div className="flex items-center justify-start gap-2">
+      <img
+        src="logo.png"
+        className="h-14 rounded-full object-cover"
+        alt="Logo"
+      />
+      <div className="flex flex-col">
+        <p className="text-black font-semibold dark:text-white">First Loser</p>
+        <p className="text-gray-500 font-medium dark:text-gray-400">
+          @why did i lose
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Head
+export default Head;
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,24 +1,151 @@
+import { Link } from "react-router-dom";
 import Head from "./Head";
 
 function Sidebar() {
   return (
-    <div className="hidden lg:grid grid-rows-4 col-span-2 bg-gray-600">
-      <div className="row-span-1">
-      <div className="relative hidden md:block">
-      <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-        </svg>
-        <span className="sr-only">Search icon</span>
+    <div className="w-full max-h-screen bg-light-background dark:bg-dark-background hidden lg:grid grid-rows-6 col-span-2 border-l border-gray-200 dark:border-gray-600">
+      <div className="h-full row-span-1 mt-6 mx-6">
+        <div className="relative hidden md:block">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg
+              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </div>
+          <input
+            type="text"
+            id="search-navbar"
+            className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search..."
+          />
+        </div>
       </div>
-      <input type="text" id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."/>
-    </div>
+      {/* Need 5 Random users unique */}
+      <div className="min-h-96 row-span-3 mt-4 mx-4">
+        <h1 className="text-2xl font-bold mb-4 text-gray-900 dark:text-white">
+          Link Up With
+        </h1>
+        <div className="w-full flex flex-col justify-center items-center gap-5">
+          <div className="w-full flex justify-between items-center">
+            <Head />
+            <Link
+              to={"/profile"}
+              className="bg-light-secondaryText pl-4 pr-5 py-2 flex justify-center items-center gap-2 rounded-xl"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-4 text-white"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
+                />
+              </svg>
+              <p className=" text-white text-base font-semibold">Link</p>
+            </Link>
+          </div>
+          <div className="w-full flex justify-between items-center">
+            <Head />
+            <Link
+              to={"/profile"}
+              className="bg-light-secondaryText pl-4 pr-5 py-2 flex justify-center items-center gap-2 rounded-xl"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-4 text-white"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
+                />
+              </svg>
+              <p className=" text-white text-base font-semibold">Link</p>
+            </Link>
+          </div>
+          <div className="w-full flex justify-between items-center">
+            <Head />
+            <Link
+              to={"/profile"}
+              className="bg-light-secondaryText pl-4 pr-5 py-2 flex justify-center items-center gap-2 rounded-xl"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-4 text-white"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
+                />
+              </svg>
+              <p className=" text-white text-base font-semibold">Link</p>
+            </Link>
+          </div>
+          <div className="w-full flex justify-between items-center">
+            <Head />
+            <Link
+              to={"/profile"}
+              className="bg-light-secondaryText pl-4 pr-5 py-2 flex justify-center items-center gap-2 rounded-xl"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-4 text-white"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
+                />
+              </svg>
+              <p className=" text-white text-base font-semibold">Link</p>
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className="row-span-2 flex justify-between items-center mt-4 mx-4">
-      <Head/>
-        <button className="bg-light-secondaryText px-6 py-2 font-medium rounded-lg text-center text-white">Link</button>
+      {/* Need 5 Tags */}
+      <div className="max-h-full row-span-2 mt-4 mx-4">
+        <h1 className="text-2xl font-bold mb-4 text-gray-900 dark:text-white">
+          Trending Links
+        </h1>
+        <div className="max-h-full flex flex-col justify-between items-start gap-2 ml-3">
+          <p className=" text-gray-900 dark:text-white">#OctopusHas3Hearts</p>
+          <p className=" text-gray-900 dark:text-white">#BananasAreBerries</p>
+          <p className=" text-gray-900 dark:text-white">#ElephantsSenseRain</p>
+          <p className=" text-gray-900 dark:text-white">
+            #HumansShareDNAWithBananas
+          </p>
+          <p className="text-gray-900 dark:text-white">#CatsHaveMoreBones</p>
+        </div>
       </div>
-      <div className="row-span-1">Bottom</div>
     </div>
   );
 }
