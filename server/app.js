37055d9a76246d59f3ce71d67aa653f39333b776
Abdelrhman <zyn6681@gmail.com>
2024-10-16 17:31:40 +0300

    fix post and make more features

--- server/app.js
+++ server/app.js
@@ -58,7 +58,7 @@ app.use(helmet.noSniff()); // X-Content-Type-Options
 // Rate Limiting
 const generalLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per window
+    max: 1000;
     message: "Too many requests from this IP, please try again later."
 });
 
